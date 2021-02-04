940592e40e6286b245e4245cf5e3147dc3fc7fbd
Bao Nguyen <hoaibaonguyen1999@gmail.com>
2021-02-04 10:25:52 -0800

    fix add.js

--- routes/add.js
+++ routes/add.js
@@ -3,12 +3,13 @@ var data = require("../data.json");
 exports.addFriend = function(request, response) {    
 	// Your code goes here
     var name = request.query.name;
+    console.log(name);
     var description = request.query.description;
     var imageURL = "http://lorempixel.com/500/500/people";
-    var newFriend = {
-        name,
-        description,
-        imageURL
+    var newFriend = { 
+        "name": name,
+        "description": description,
+        "imageURL": imageURL
     }
     data.friends.push(newFriend);
     response.render('index', data);
